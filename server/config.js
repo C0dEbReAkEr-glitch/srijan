ad4f6522266b57ca25262a7bffee8c2d6bc77eee
3FOXshield1 <mail4khemendra@gmail.com>
2025-03-30 22:35:28 +0530

    Update deployment configuration and clean up

--- server/config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  pusher: {
-    appId: process.env.PUSHER_APP_ID || '#',
-    key: process.env.PUSHER_KEY || '#',
-    secret: process.env.PUSHER_SECRET || '#',
-    cluster: process.env.PUSHER_CLUSTER || '#'
-  }
-};
